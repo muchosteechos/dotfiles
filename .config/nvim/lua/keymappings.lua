5d55f55303f93c5a01c09966b83e7ce51edb9f6d
Steen Sziegaud <s.sziegaud@gmail.com>
2021-03-15 19:14:05 +0100

    Add split navigation mappings

---
+++ .config/nvim/lua/keymappings.lua
@@ -0,0 +1,15 @@
+-- EXAMPLE
+-- vim.api.nvim_set_keymap('n', '<Leader><Space>', ':set hlsearch!<CR>', { noremap = true, silent = true })
+-- :nnoremap <silent> <Leader><Space> :set hlsearch<CR>
+-- vim.api.nvim_set_keymap('n', '<Leader>tegf',  [[<Cmd>lua require('telescope.builtin').git_files()<CR>]], { noremap = true, silent = true })
+-- :nnoremap <silent> <Leader>tegf <Cmd>lua require('telescope.builtin').git_files()<CR>
+
+-- vim.api.nvim_buf_set_keymap(0, '', 'cc', 'line(".") == 1 ? "cc" : "ggcc"', { noremap = true, expr = true })
+-- :noremap <buffer> <expr> cc line('.') == 1 ? 'cc' : 'ggcc'
+
+
+-- split navigation
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
