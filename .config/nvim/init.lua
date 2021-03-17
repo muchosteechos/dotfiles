81750d61b0a1c2c4b71cf5af4094bcc08e6da202
Steen Sziegaud <s.sziegaud@gmail.com>
2021-03-17 23:05:57 +0100

    nvim: reinstall neovim to resolve tree-sitter errors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
+require('plugins') -- should be loaded first
 require('colorscheme')
 require('options')
 require('variables')
 require('keymappings')
-require('plugins')
+
+require'lspconfig'.pyright.setup{}
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,7 +10,6 @@ end
 
 execute 'packadd packer.nvim'
 
-
 return require('packer').startup(function()
   -- packer can manage itself as an optional plugin
   use {'wbthomason/packer.nvim', opt = true}
@@ -20,10 +19,17 @@ return require('packer').startup(function()
   -- lsp built-arounds
 
   -- Treesitter
-  use {'nvim-treesitter/nvim-treesitter', run=':TSUpdate'} -- treesitter config and abstraction layer
+  use {
+    'nvim-treesitter/nvim-treesitter', 
+    run = ':TSUpdate',
+    config = function() require'c-treesitter' end,
+  } -- treesitter config and abstraction layer
 
   -- File Explorer
-  use {'ms-jpq/chadtree', branch = 'chad', run='python3 -m chadtree deps'}
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {'kyazdani42/nvim-web-devicons'},
+  }
 
   -- Git Integration
   use {'TimUntersberger/neogit'} -- WIP Magit clone for neovim
@@ -37,8 +43,15 @@ return require('packer').startup(function()
   use {'Th3Whit3Wolf/space-nvim'} -- space-nvim with treesitter support
 
   -- Status- & Tabline
-  use {'glepnir/galaxyline.nvim'}
-  use {'romgrk/barbar.nvim'}
+  use {
+    'glepnir/galaxyline.nvim', 
+    config = function() require'c-galaxyline' end,
+    requires = {'kyazdani42/nvim-web-devicons'}
+  }
+  use {
+    'romgrk/barbar.nvim',
+    config = function() require'c-barbar' end
+  }
 
   -- Simple quality of life improvements
   use {'glepnir/prodoc.nvim'} -- comment and annotation plugin
